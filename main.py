3bc57fea50e91e990d91b9700c97be58b560cb2c
YogiK2001 <kaustubh7718@gmail.com>
2024-09-08 01:28:28 +0530

    day1

---
+++ main.py
@@ -0,0 +1,22 @@
+#Basic Python --> Fundamental Data Types
+# print(type(2+4))
+# print(type(2-4))
+# print(type(2*4))
+# print(type(2/4)) #0.5
+
+# https://www.youtube.com/watch?v=PZRI1IfStY0
+
+# print(2 ** 3) #Power
+# print(3 // 4) #Returns Int
+# print(5 % 4)
+
+#Math Functions
+
+print(round(3.1))
+print(abs(round(3.1)))
+
+# https://www.programiz.com/python-programming/modules/math
+
+#Operator Presedence
+
+
